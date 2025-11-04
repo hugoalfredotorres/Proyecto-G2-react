b726c8932b7f537ae033fae9bd18a1412b6c1b1e
Estefania Cabello <estefaniacabello24@gmail.com>
2025-11-04 20:06:42 -0300

    corrignedo error de context en dashboard doctor

--- src/pages/doctor/Dashboard.jsx
+++ src/pages/doctor/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { AppContext } from "../context/AppContext";
+import { AppContext } from "../../context/AppContext";
 
 const Dashboard = () => {
   const { appointments, updateAppointmentStatus } = useContext(AppContext);
